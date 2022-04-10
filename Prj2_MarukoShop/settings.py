4015ad720be66aa12baa33b4097901a6f7cf7b98
Giang Nguyen Van <giangnv.soict.hust@gmail.com>
2022-04-10 11:49:53 +0700

    Chuyen code sang sqlite

--- Prj2_MarukoShop/settings.py
+++ Prj2_MarukoShop/settings.py
@@ -79,15 +79,10 @@ WSGI_APPLICATION = 'Prj2_MarukoShop.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
- 'default': {
- 'ENGINE': 'django.db.backends.mysql',
- 'NAME': 'Maruko',
- 'USER':'root',
- 'PASSWORD':'root2907',
- 'HOST':'localhost',
- 'PORT': '3306',
- 'OPTIONS': {'charset': 'utf8mb4'}
- }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
